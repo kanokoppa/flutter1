020d146350b0e45a56e812fe91345ac7bc09a23b
kanokoppa <piyopiyokekeshi@gmail.com>
2020-08-26 18:39:41 +0900

    見た目の調整

--- lib/contributors_detail.dart
+++ lib/contributors_detail.dart
@@ -39,13 +39,285 @@ class _ContributorsDetailState extends State<ContributorsDetail> {
   ) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.url),
+        title: new Text("Contributorsの詳細"),
+        backgroundColor: Colors.teal,
       ),
-      body: Column(
-        children: <Widget>[
-
-          _detail != null ? Text(_detail['name']) : Text('読み込み中'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    "名前: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                _detail != null
+                    ? Text(
+                        _detail['name'].toString(),
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      )
+                    : Text('読み込み中'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "ユーザーID: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['login'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "フォロワー数: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['followers'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "フォロー数: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['following'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "会社: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['company'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "ロケーション: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['location'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "パブリックリポジトリ数: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['public_repos'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    " パブリックgist数: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          _detail['public_gists'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "Twitterアカウント: @",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          "" + _detail['twitter_username'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "GitHubのURL: ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  _detail != null
+                      ? Text(
+                          "" + _detail['html_url'].toString(),
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        )
+                      : Text('読み込み中'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       //child: Text(_jsonData[index]['name'].toString())),
     );
